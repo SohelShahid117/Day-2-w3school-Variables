8e340ca9857d38eea957ea03e7dd45e95bae0028
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-11 22:29:49 +0600

    now enjoy from w3chool cpp tutorial variables part

---
+++ variable.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //C++ Variables
+
+    cout<<"hello world\n";
+
+
+    return 0;
+}
+
+/*
+https://www.w3schools.com/cpp/cpp_variables.asp
+
+*/
