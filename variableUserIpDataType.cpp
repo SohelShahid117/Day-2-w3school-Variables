a7e3a5d340a212fcdffee33782c1d73290c6e98f
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-14 23:00:45 +0600

    upto C++ Data Types completely done

--- variableUserIpDataType.cpp
+++ variableUserIpDataType.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
+#include<string>
 using namespace std;
 int main(){
 
-    //C++ Variables,Multiple Variables,Identifiers,Constants,Real-Life Examples
-    //C++ User Input
-    
+    /* C++ Variables,Multiple Variables,Identifiers,Constants,Real-Life Examples,User Input,Data Types-Numeric,Boolean,Character,String   Data Types
+    */
+
     // int myNum =22;
     int myNum;
     myNum=32;
@@ -20,7 +21,6 @@ int main(){
 
     int x = 5;
     int y =19;
-    //hello
     int sum=x+y;
     cout<<"sum = "<<sum<<endl;
 
@@ -71,10 +71,57 @@ cin>>width;
 int area=length*width;
 cout<<"area of rectangle = "<<area<<"\n";
 
-   
+int myIntNum =99;
+float myFloatNum = 67.99;
+double myDoubleNum = 89.98876;
+char myLetter = 'A';
+bool myBoolean = true;
+string myString = "how r u?";
+
+cout<<myIntNum<<"\n";
+cout<<myFloatNum<<"\n";
+cout<<myDoubleNum<<"\n";
+// cout<<myIntNum<<"\n";
+cout<<myLetter<<"\n";
+cout<<myBoolean<<"\n";
+cout<<myString<<"\n";
+
+// float f1 = 1.2e6;
+float f1 = 1.2e3;
+// double d1 = 1.2e17;
+double d1 = 1.2e7;
+cout<<f1<<"\n";
+cout<<d1<<"\n";
+
+bool isCodingFun = true;
+bool isCodeCopyGood = false;
+cout<<isCodingFun<<"\n";
+cout<<isCodeCopyGood<<"\n";
+
+char a1 = 71,b1=99,c1=5;
+cout<<a1<<"\n";
+cout<<b1<<"\n";
+cout<<c1<<"\n";
+
+int items;
+cout<<"enter the number of items\n";
+cin>>items;
+
+float costPerItems;
+cout<<"enter the cost of per items\n";
+cin>>costPerItems;
+
+double totalCost = items*costPerItems;
+
+char currency = '$';
+
+cout<<"number of items "<<items<<"\n";
+cout<<"cost per items "<<costPerItems<<currency<<"\n";
+cout<<"total cost "<<totalCost<<currency<<"\n";
 
 
-    return 0;
+
+return 0;
 }
 
 /*
@@ -87,4 +134,36 @@ https://www.w3schools.com/cpp/cpp_user_input.asp
 4.When you do not want others (or yourself) to change existing variable values, use the const keyword (this will declare the variable as "constant", which means unchangeable and read-only).
 5.When you declare a constant variable, it must be assigned with a value
 
+6.
+Data Type	     Size	    Description
+
+boolean	        1 byte	    Stores true or false values
+char	        1 byte	    Stores a single character/letter/number, or ASCII values
+int	            2 or        Stores whole numbers, without decimals
+                4 bytes	
+
+float	        4 bytes	    Stores fractional numbers, containing one or more decimals.
+                            Sufficient for storing 6-7 decimal digits          
+double	        8 bytes	    Stores fractional numbers, containing one or more decimals.
+                            Sufficient for storing 15 decimal digits.
+
+
+7.float vs. double
+
+The precision of a floating point value indicates how many digits the value can have after the decimal point. The precision of float is only six or seven decimal digits, while double variables have a precision of about 15 digits. Therefore it is safer to use double for most calculations.
+
+8.Boolean values are mostly used for conditional testing
+9.The char data type is used to store a single character. The character must be surrounded by single quotes, like 'A' or 'c':
+
+10.To use strings, you must include an additional header file in the source code, the <string> library:
+
 */
+
+/**
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/SohelShahid117/Day3-w3school-cpp-operator-string-math-boolean.git
+git push -u origin main
+ */
